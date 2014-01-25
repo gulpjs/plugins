44b00169419bcfcb7f0860e4e1ab01668695034e
gratimax <max@ovsankin.com>
2014-01-25 13:12:02 -0800

    loading is now faster(two requests)

--- scripts/controller/pluginList.js
+++ scripts/controller/pluginList.js
@@ -1,17 +1,25 @@
 'use strict';
 
 angular.module('npm-plugin-browser')
-    .controller('PluginListCtrl', function ($scope, $http) {
-      $http.get('http://npmsearch.com/query', {
+  .controller('PluginListCtrl', function ($scope, $http) {
+
+    var makeRequest = function (start, size) {
+      return $http.get('http://npmsearch.com/query', {
         params: {
           q: 'keywords:gulpplugin,gulpfriendly',
           fields: 'name,keywords,rating,description,author,modified,homepage,version,license',
-          start: 0,
-          size: 10000,
+          start: start,
+          size: size,
           sort: 'rating:desc'
+        }});
+    };
 
-        }})
-          .then(function (response) {
-            $scope.data = response.data;
-          });
+    // make first request for quick load
+    makeRequest(0, 15).then(function (response) {
+      $scope.data = response.data.results;
+      // load everything fully
+      makeRequest(0, response.data.total).then(function (response) {
+        $scope.data = response.data.results;
+      });
     });
+  });
