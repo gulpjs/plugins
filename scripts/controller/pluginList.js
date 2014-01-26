be4c3339ecdf086cd8f06a2c4b8aeee2f5d74149
gratimax <max@ovsankin.com>
2014-01-26 15:15:45 -0800

    Colorized plugin keywords, better search counting

--- scripts/controller/pluginList.js
+++ scripts/controller/pluginList.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('npm-plugin-browser')
-  .controller('PluginListCtrl', function ($scope, $http) {
+  .controller('PluginListCtrl', function ($scope, $http, $filter) {
+
+    var total = 0;
 
     var makeRequest = function (start, size) {
       return $http.get('http://npmsearch.com/query', {
@@ -14,12 +16,43 @@ angular.module('npm-plugin-browser')
         }});
     };
 
+    $scope.numPlugins = 0;
+    $scope.search = '';
+
+    // moves and colorizes some keywords
+    var handleKeywords = function (plugin) {
+      var keywords = plugin.keywords;
+
+      var kindex = keywords.indexOf('gulpfriendly');
+      if(kindex != -1) {
+        keywords.splice(kindex, 1);
+        keywords.unshift({name: 'gulpfriendly', color: 'success'});
+      }
+
+      var pindex = keywords.indexOf('gulpplugin');
+      if(pindex != -1) {
+        keywords.splice(pindex, 1);
+        keywords.unshift({name: 'gulpplugin', color: 'info'});
+      }
+
+      plugin.keywords = plugin.keywords.map(function (keyword) {
+        if(!keyword.name) {
+          return {name: keyword, color: 'default'};
+        } else return keyword;
+      });
+
+      return plugin;
+    };
+
     // make first request for quick load
     makeRequest(0, 15).then(function (response) {
-      $scope.data = response.data.results;
+      $scope.data = response.data.results.map(handleKeywords);
+      total = response.data.total;
+      $scope.numPlugins = total;
       // load everything fully
-      makeRequest(0, response.data.total).then(function (response) {
-        $scope.data = response.data.results;
+      makeRequest(0, total).then(function (response) {
+        $scope.data = response.data.results.map(handleKeywords);
+        $scope.numPlugins = $filter('filter')($scope.data, $scope.search).length;
       });
     });
   });
