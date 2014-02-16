0376185b9bc65fae743aca673fae0f23d15e83aa
Robin Böhm <robinboehm@googlemail.com>
2014-02-16 23:22:29 +0100

    add a tooltip directive

---
+++ app/js/directive/tooltip.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('npm-plugin-browser')
+    .directive('tooltip', function () {
+      return {
+        link: function (scope, element, attrs) {
+          element
+              .attr('title', attrs.tooltip)
+              .tooltip({placement: 'right'});
+        }
+      }
+    });
