a1c74dc7f2cdb1780399499e4552ebc60b985d72
Sindre Sorhus <sindresorhus@gmail.com>
2014-02-17 20:23:59 +0100

    blacklist - gulp-myth-css
    https://github.com/worldeggplant/gulp-myth/issues/1

--- app/js/value/blackList.js
+++ app/js/value/blackList.js
@@ -3,5 +3,6 @@
 angular.module('npm-plugin-browser')
     .value('blackList', {
       'gulp-browserify': 'use the browserify module directly',
-      'gulp-requirejs': 'use the require.js module directly'
+      'gulp-requirejs': 'use the require.js module directly',
+      'gulp-myth-css': 'duplicate of gulp-myth'
     });
