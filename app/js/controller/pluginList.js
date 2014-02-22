d70dc5fb0ab3e2b200877c2576e015cbef79d26b
Robin Böhm <robinboehm@googlemail.com>
2014-02-22 16:10:33 +0100

    some refactoring in fetch request promise chain

--- app/js/controller/pluginList.js
+++ app/js/controller/pluginList.js
@@ -3,8 +3,6 @@
 angular.module('npm-plugin-browser')
     .controller('PluginListCtrl', function ($scope, $http, $location, ngProgress, blackList) {
 
-      $scope.blackList = blackList;
-
       var makeRequest = function (start, size) {
         return $http.get('http://npmsearch.com/query', {
           params: {
@@ -14,25 +12,30 @@ angular.module('npm-plugin-browser')
             size: size,
             sort: 'rating:desc'
           }});
-      };
+      },
+          initialFetchSize = 15;
+
+      $scope.blackList = blackList;
 
       // make first request for quick load
       ngProgress.start();
-      makeRequest(0, 15)
+      makeRequest(0, initialFetchSize)
           .then(function (response) {
             $scope.data = response.data.results;
             return response.data.total;
           })
           .then(function (total) {
-            return makeRequest(0, total);
+            return makeRequest(initialFetchSize, total);
           })
           .then(function (response) {
-            $scope.data = response.data.results;
+            $scope.data =  $scope.data.concat(response.data.results);
+          })
+          .then(function(){
             if (angular.isString(($location.search()).q)) {
               $scope.search = ($location.search()).q;
             }
-            ngProgress.complete();
-          });
+          })
+          .then(ngProgress.complete);
 
       $scope.orderByGulpKeywords = function (item) {
         return (item === 'gulpplugin' || item === 'gulpfriendly') ? -1 : 0;
