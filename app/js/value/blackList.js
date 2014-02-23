f28539d5bf70630efaa1cc05edf3117639933ef1
Sindre Sorhus <sindresorhus@gmail.com>
2014-02-23 12:06:12 +0100

    Update blackList.js

--- app/js/value/blackList.js
+++ app/js/value/blackList.js
@@ -6,5 +6,6 @@ angular.module('npm-plugin-browser')
       'gulp-requirejs': 'use the require.js module directly',
       'gulp-myth-css': 'duplicate of gulp-myth',
       'gulp-filesize': 'duplicate of gulp-size',
-      'gulp-redust': 'duplicate of gulp-dust'
+      'gulp-redust': 'duplicate of gulp-dust',
+      'gulp-shell': 'duplicate of gulp-exec'
     });
