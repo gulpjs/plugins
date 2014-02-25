823d2408c24f1f4082dad95be4207b92f3aa4f7a
Blaine Bublitz <blaine@iceddev.com>
2014-02-24 18:26:59 -0700

    Update blackList.js

--- app/js/value/blackList.js
+++ app/js/value/blackList.js
@@ -7,5 +7,17 @@ angular.module('npm-plugin-browser')
       'gulp-myth-css': 'duplicate of gulp-myth',
       'gulp-filesize': 'duplicate of gulp-size',
       'gulp-redust': 'duplicate of gulp-dust',
-      'gulp-shell': 'duplicate of gulp-exec'
+      'gulp-shell': 'duplicate of gulp-exec',
+      'gulp-compile-js': 'combines other plugins',
+      'gulp-module-system': 'does too much, use gulp-wrap-* modules',
+      'gulp-wrap-define': 'duplicate of gulp-wrap-amd',
+      'gulp-urequire': 'no documentation',
+      'gulp-absolute': 'use gulp-filter and node\'s path module',
+      'gulp-wintersmith': 'use the wintersmith module directly',
+      'gulp-connect': 'use the connect module directly',
+      'gulp-image-optimization': 'duplicate of gulp-imagemin',
+      'gulp-bower': 'use the bower module directly',
+      'gulp-ember-handlebars': 'duplicate of gulp-handlebars & too complex',
+      'gulp-handlebars-michael': 'duplicate of gulp-handlebars',
+      'gulpify': 'deprecated - use vinyl-source-stream instead'
     });
