8106e6a8746e46378dc465f1b61f36efd917ae78
Robin Böhm <robinboehm@googlemail.com>
2014-03-23 00:58:48 +0100

    refactor build
    usemin
    gulp nativ

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,44 +1,48 @@
-var gulp = require('gulp');
-var clean = require('gulp-clean');
-var brunch = require('brunch');
-var runseq = require('run-sequence');
+var gulp = require('gulp'),
+  clean = require('gulp-clean'),
+  uglify = require('gulp-uglify'),
+  minifyCss = require('gulp-minify-css'),
+  ngmin = require('gulp-ngmin'),
+  useref = require('gulp-useref'),
+  filter = require('gulp-filter');
+
 require('gulp-grunt')(gulp);
 
 // Clean public
 gulp.task('clean', function () {
-  return gulp.src('public/', {read: false})
+  return gulp.src('dist/', {read: false})
     .pipe(clean());
 });
 
-// Push using grunt-gh-pages
-gulp.task('gh-pages', function (cb) {
-  gulp.run('grunt-gh-pages', cb);
+// Build
+gulp.task('default', ['clean', 'build']);
+
+gulp.task('build', ['assets'], function () {
+  var jsFilter = filter('**/*.js');
+  var cssFilter = filter('**/*.css');
+
+  return gulp.src('src/index.html')
+    .pipe(useref.assets())
+    .pipe(jsFilter)
+    .pipe(ngmin())
+    .pipe(uglify({outSourceMaps: true}))
+    .pipe(jsFilter.restore())
+    .pipe(cssFilter)
+    .pipe(minifyCss())
+    .pipe(cssFilter.restore())
+    .pipe(useref.restore())
+    .pipe(useref())
+    .pipe(gulp.dest('dist'));
 });
 
-// brunch build --production
-gulp.task('build:prod', function (cb) {
-  brunch.build({production: true}, function () {
-    cb();
-  });
+gulp.task('assets', function () {
+  return gulp.src(['src/blackList.json', 'src/README.md'])
+    .pipe(gulp.dest('dist/'))
+    .pipe(filter('src/fonts/**/*'))
+    .pipe(gulp.dest('dist/fonts'));
 });
 
 // Deploy
-gulp.task('deploy', function (cb) {
-  // ugly hack to run in sequence
-  runseq(
-    'clean',
-    'build:prod',
-    'gh-pages',
-    cb);
-});
-
-// Simple dev task, brunch watch --server
-gulp.task('dev', function (cb) {
-  // another ugly hack
-  runseq(
-    'clean',
-    function () {
-      brunch.watch({server: true});
-    }
-  );
+gulp.task('deploy', ['default'], function (cb) {
+  gulp.run('grunt-gh-pages', cb);
 });
